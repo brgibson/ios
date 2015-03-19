3ec908a1b753769c6ba44298af0cfd74984a24c7
Ben Gibson <brgibson@indiana.edu>
2015-03-19 11:10:58 -0700

    Pitch Perfect - after lesson two - creating buttons - hiding/showing buttons on click - outlets and actions

--- Pitch Perfect/Pitch Perfect/ViewController.swift
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Record the users voice
+        println("recording voice")
+        recordingInProgress.hidden = false
+        stopRecordingButton.hidden = false
+    }
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingInProgress.hidden = true
+        stopRecordingButton.hidden = true
+    }
 }
 
